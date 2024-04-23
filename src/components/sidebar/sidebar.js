8b7bf6b3f3cbaf8bfd0f65b9c7f8ccdf705604b6
Naol <37399395+Belayne@users.noreply.github.com>
2024-04-23 10:24:59 +0200

    Add current weather data to sidebar

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,36 +1,61 @@
 import "./sidebar.css";
 import {format} from "date-fns";
 
-export default function makeSidebar(weatherData) {
-    const view = document.createElement('div');
-    view.id = "sidebarContainer";
+function getTime(date) {
+    return new Date(date).toLocaleTimeString([], { hour: "2-digit", minute: "2-digit", hour12: false });
+}
 
-    const sidebarDateLocation = new DateLocation(weatherData);
-    view.appendChild(sidebarDateLocation.view)
-    
+function createDateLocationView(location, lastUpdate) {
+    let time = getTime(lastUpdate)
+    const date = format(new Date, 'eeee d MMMM');
+    const view = document.createElement('div');
+
+    view.classList.add('dateLocationDiv')
+    view.innerHTML = /*html*/ `
+        <h1 class="sidebarLocation">${location}</h1>
+        <p class="sidebarTime">${time}</p>
+        <p class="sidebarDate">${date}</p>
+    `
 
     return view;
 }
 
-class DateLocation {
-    constructor(data) {
-        this.time = format(data.date, 'HH:MM');
-        this.date = format(data.date, 'eeee d MMMM');
-        this.location = data.location;
+function createWeatherView(weatherData, american = false) {
+    const view = document.createElement('div');
+    let weatherText = weatherData.condition.text;
+    let weatherIcon = weatherData.condition.icon;
+    let temp = (american)? weatherData.temp_f + "°F": weatherData.temp_c + "°C";
+    let feelTemp = (american)? weatherData.feelslike_f + "°F": weatherData.feelslike_c + "°C";
+    
+    view.classList.add('sidebarWeather');
+    view.innerHTML = /*html*/ `
+        <figure class="sidebarWeatherFig">
+            <img src="${weatherIcon}"/>
+            <figcaption>${weatherText}</figcaption>
+        </figure>
+        <p class="sidebarTemp">${temp}</p>
+        <p class="sidebarPerceived">Perceived ${feelTemp}</p>
+    `
 
-        this.view = document.createElement('div');
-        this.view.classList.add('dateLocationDiv')
-
-        this.view.innerHTML = /*html*/ `
-            <h1 class="sidebarLocation">${this.location}</h1>
-            <p class="sidebarTime">${this.time}</p>
-            <p class="sidebarDate">${this.date}</p>
-        `
+    function update(newData) {
+        
     }
 
-    update(newData) {
-        this.date = format(newData.date, 'HH:MM');
-        this.time = format(newData.date, 'eeee d MMMM');
-        this.location = newData.location;
-    }
+    return view;
 }
+
+
+export default function makeSidebar(weatherData) {
+    const view = document.createElement('div');
+    view.id = "sidebarContainer";
+
+    const sidebarDateLocation = createDateLocationView(weatherData.location, weatherData.now.last_updated);
+    const sidebarWeather = createWeatherView(weatherData.now);
+
+    view.append(sidebarDateLocation);
+    view.append(sidebarWeather);
+
+    return view;
+}
+
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { getWeather } from "./weatherData"
 import makeSidebar from "./components/sidebar/sidebar";
 
 async function log() {
-    const weather = await getWeather("London");
+    const weather = await getWeather("Rome");
     console.log(weather)
 }
 
@@ -10,7 +10,7 @@ async function controller() {
     const weather = await getWeather("Rome");
     const sidebar = makeSidebar(weather);
 
-    document.querySelector("#container").appendChild(sidebar)
+    document.querySelector("#container").append(sidebar)
 }
 
 log();
